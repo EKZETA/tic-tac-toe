0babeb8970afe3f906c24e09327cdd309d55dd4f
Ekzo Vincenzo <ekopriambodo12gt@gmail.com>
2025-07-25 19:54:50 +0700

    Make the board 3x3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 
-function App() {
+function Square() {
+  return <button className="square">X</button>
+}
 
+export default function Board() {
   return (
-    <>
-      
-    </>
+    <div className="board">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
   )
 }
 
-export default App
+
